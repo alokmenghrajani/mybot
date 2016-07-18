13459d6983bc2e01c43cb8ab81873c0806be1537
Cedric Staub <cs@squareup.com>
2016-07-18 16:41:49 -0700

    Random fixes

--- debug.go
+++ debug.go
@@ -20,13 +20,25 @@ func debug(config Config, ws *websocket.Conn, user string, channel string, cmd s
 	run.Stdout = &stdout
 	run.Stderr = &stderr
 	err := run.Run()
-	if err != nil {
-		m.Text = fmt.Sprintf("<@%s>: failed running %s, %s\n```stdout: %s```\nstderr: ```%s```\n", user, run, err, stdout.String(), stderr.String())
-	} else {
-		m.Text = fmt.Sprintf("<@%s>: ```%s```\nstderr: ```%s```\n", user, stdout.String(), stderr.String())
+	stdoutStr := stdout.String()
+	stderrStr := stderr.String()
+	cutOff := false
+	if len(stdoutStr) > 256 {
+		cutOff = true
+		stdoutStr = stdoutStr[:256]
 	}
-	if len(m.Text) > 4096 {
-		m.Text = m.Text[:4096]
+	if len(stderrStr) > 256 {
+		cutOff = true
+		stderrStr = stderrStr[:256]
+	}
+	if err != nil {
+		m.Text = fmt.Sprintf("<@%s>: failed running %s, %s\nstdout: ```%s```\nstderr: ```%s```\n", user, cmd, err, stdoutStr, stderrStr)
+	} else {
+		m.Text = fmt.Sprintf("<@%s>: ```%s```\nstderr: ```%s```\n", user, stdoutStr, stderrStr)
+	}
+	postMessage(ws, m)
+	if cutOff {
+		m.Text = "(message too long; some parts truncated)"
 	}
 	postMessage(ws, m)
 }
